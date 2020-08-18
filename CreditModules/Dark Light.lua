3c2ab0ade36009c3b6e57b45ba376703d15c8734
Simply <20605517+SimplyRekt@users.noreply.github.com>
2020-08-18 17:29:36 -0400

    Add CreditModules
    Shows user's headshots as well as their username and the theme they created.

---
+++ CreditModules/Dark Light.lua
@@ -0,0 +1,18 @@
+local Players = game:GetService("Players")
+local UserID = 15486697;
+
+local function GetUserInfo(ID)
+	local URL, Ready = Players:GetUserThumbnailAsync(ID, Enum.ThumbnailType.AvatarBust, Enum.ThumbnailSize.Size100x100);
+	local Username = Players:GetNameFromUserIdAsync(ID)
+	if Ready and Username then
+		return {URL, Username}
+	elseif not Ready and Username then
+		return {"rbxassetid://8167273", Username}
+	elseif Ready and not Username then
+		return {Ready, "UNKNOWN (error)"}
+	elseif not Ready and not Username then
+		return {"rbxassetid://8167273", "UNKNOWN (error)"}
+	end
+end
+
+return GetUserInfo(UserID)
---
+++ CreditModules/Easy On the Eyes.lua
@@ -0,0 +1,17 @@
+local Players = game:GetService("Players")
+local UserID = 289025524;
+
+local function GetUserInfo(ID)
+	local URL, Ready = Players:GetUserThumbnailAsync(ID, Enum.ThumbnailType.AvatarBust, Enum.ThumbnailSize.Size100x100);
+	local Username = Players:GetNameFromUserIdAsync(ID)
+	if Ready and Username then
+		return {URL, Username}
+	elseif not Ready and Username then
+		return {"rbxassetid://8167273", Username}
+	elseif Ready and not Username then
+		return {Ready, "UNKNOWN (error)"}
+	elseif not Ready and not Username then
+		return {"rbxassetid://8167273", "UNKNOWN (error)"}
+	end
+end
+return GetUserInfo(UserID)
